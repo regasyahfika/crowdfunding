15594bc7c6e23faf5756024600f0ce708282f0e8
Rega Syahfika <rega.syahfika@gmail.com>
2022-06-24 18:28:52 +0700

    add handler campaign

--- handler/campaign.go
+++ handler/campaign.go
@@ -1,1 +1,35 @@
 package handler
+
+import (
+	"crowdfunding/campaign"
+	"crowdfunding/helper"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type campaignHandler struct {
+	service campaign.Service
+}
+
+func NewHandler(service campaign.Service) *campaignHandler {
+	return &campaignHandler{service}
+}
+
+func (h *campaignHandler) GetCampaigns(c *gin.Context) {
+	userID, _ := strconv.Atoi(c.Query("user_id"))
+
+	campaigns, err := h.service.GetCampaigns(userID)
+
+	if err != nil {
+		response := helper.APIReseponse("Error to get campaigns", http.StatusBadRequest, "error", nil)
+
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.APIReseponse("List of campaigns", http.StatusOK, "Success", campaigns)
+	c.JSON(http.StatusOK, response)
+	return
+}
